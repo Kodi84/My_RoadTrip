94622e82487e7a4298ace90b7c56a435ede9d4a3
Cung Tran <cung.n.tran@gmail.com>
2017-06-04 13:50:29 -0700

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,14 +1,3 @@
-// Create a "close" button and append it to each list item
-// var myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//     var span = document.createElement("SPAN");
-//     var txt = document.createTextNode("\u00D7");
-//     span.className = "close";
-//     span.appendChild(txt);
-//     myNodelist[i].appendChild(span);
-// }
-
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("delete");
 var i;
@@ -18,12 +7,6 @@ for (i = 0; i < close.length; i++) {
         div.style.display = "none";
     }
 }
-
-
-
-
-
-
 // Create a new list item when clicking on the "Add" button
 function newElement() {
     var li = document.createElement("li");
